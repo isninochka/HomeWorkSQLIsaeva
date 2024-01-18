72e09563e4bc0ec643b373b388d06fdb525ca704
isninochka <96066722+isninochka@users.noreply.github.com>
2024-01-18 09:32:45 +0100

    Update HomeWork11_01_24.sql

--- HomeWork11_01_24.sql
+++ HomeWork11_01_24.sql
@@ -23,6 +23,7 @@ FROM OrderDetails
 JOIN Products ON OrderDetails.ProductID = Products.ProductID
 GROUP BY OrderID
 ORDER BY min_order_sum
+LIMIT 3  
 
 -- 4. Вывести имена и фамилии (и ко-во заказов) менеджеров, у которых ко-во заказов менее 15
 -- (не было таких заказов, поэтому сделала больше 15)
